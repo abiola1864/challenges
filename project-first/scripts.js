b9a4a5387cab0472f6a0c3a860072cc38b1233eb
abiola1864 <oyebanjoabiola@gmail.com>
2023-06-03 16:55:15 +0200

    made project one a flex grid

--- project-first/scripts.js
+++ project-first/scripts.js
@@ -1,20 +1,29 @@
 const firstChoiceButton = document.querySelector('[data-js="choice-button-1"]');
 const AnswerButton = document.querySelector('[data-js="active-button"]');
-const originalTextElement = firstChoiceButton.querySelector('.item-text-cbox');
-const conditionalTextElement = firstChoiceButton.querySelector('.text-conditional');
+const originalTextElement = document.querySelector('.item-text-cbox');
+const conditionalTextElement = document.querySelector('.text-conditional');
 
 firstChoiceButton.addEventListener("click", () => {
   firstChoiceButton.classList.toggle("dark");
-  AnswerButton.classList.add("dark");
+  AnswerButton.classList.toggle("dark");
+
+     // allows text content to be the content I set in the
+    // html element called data-original-text
+    // originalTextElement.textContent = originalTextElement.dataset.originalText;
   
+    
+  originalTextElement.textContent = originalTextElement.dataset.originalText;
+  
+  
+  // eventlistener for the color and text toggles
+
 
   if (!firstChoiceButton.classList.contains("dark")) {
     // Show the conditional text and hide the original text
     originalTextElement.style.display = "none";
     conditionalTextElement.style.display = "block";
-    originalTextElement.textContent = originalTextElement.dataset.originalText;
-  
-    
+
+ 
     AnswerButton.classList.remove("dark");
   } else {
     // Show the original text and hide the conditional text
@@ -24,3 +33,25 @@ firstChoiceButton.addEventListener("click", () => {
     
   }
 });
+
+
+
+
+
+window.addEventListener('load', function() {
+  adjustFooterWidth();
+});
+
+window.addEventListener('resize', function() {
+  adjustFooterWidth();
+});
+
+function adjustFooterWidth() {
+  var containerWidth = document.getElementById('container').offsetWidth;
+  var footer = document.querySelector('.footer');
+  footer.style.width = containerWidth + 'px';
+}
+
+
+
+
