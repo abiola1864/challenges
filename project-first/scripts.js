7c7f95f0d3ca3d7b0cc52dc48e39f15b96cbc6bb
abiola1864 <oyebanjoabiola@gmail.com>
2023-06-03 22:18:27 +0200

    added randomized quiz

--- project-first/scripts.js
+++ project-first/scripts.js
@@ -2,55 +2,127 @@ const firstChoiceButton = document.querySelector('[data-js="choice-button-1"]');
 const AnswerButton = document.querySelector('[data-js="active-button"]');
 const originalTextElement = document.querySelector('.item-text-cbox');
 const conditionalTextElement = document.querySelector('.text-conditional');
+const quizContentElements = document.querySelectorAll('.item-text');
+const answerOptionElements = document.querySelectorAll('.tag-container span');
+
+let quizData = []; // Initialize an empty array to store quiz data
+let currentIndex = -1; // Initialize the current quiz line index to -1
+let updateTimeout = null; // Store the timeout reference
+
+// Function to choose the next quiz line and update the content and answer options
+function updateQuiz() {
+  // Increment the current index
+  currentIndex = (currentIndex + 1) % quizData.length;
+
+  // Get the quiz line at the current index
+  const quizLine = quizData[currentIndex];
+
+  // Randomize the order of answer options
+  const answerOptions = [
+    quizLine.tag1,
+    quizLine.tag2,
+    quizLine.tag3
+  ].sort(() => Math.random() - 0.5);
+
+  // Update the quiz content and randomized answer options
+  quizContentElements.forEach((contentElement) => {
+    contentElement.textContent = quizLine.content;
+  });
+
+  answerOptionElements.forEach((answerOptionElement, index) => {
+    answerOptionElement.textContent = answerOptions[index];
+  });
+}
+
+// Load the quiz data from the CSV file
+fetch('quiz.csv')
+  .then(response => response.text())
+  .then(data => {
+    quizData = Papa.parse(data, { header: true }).data;
+    updateQuiz(); // Initial update of quiz content and answer options
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
 
 firstChoiceButton.addEventListener("click", () => {
   firstChoiceButton.classList.toggle("dark");
   AnswerButton.classList.toggle("dark");
-
-     // allows text content to be the content I set in the
-    // html element called data-original-text
-    // originalTextElement.textContent = originalTextElement.dataset.originalText;
+  //      // allows text content to be the content I set in the
+//     // html element called data-original-text
+ originalTextElement.textContent = originalTextElement.dataset.originalText;
   
-    
-  originalTextElement.textContent = originalTextElement.dataset.originalText;
-  
-  
-  // eventlistener for the color and text toggles
-
 
   if (!firstChoiceButton.classList.contains("dark")) {
-    // Show the conditional text and hide the original text
     originalTextElement.style.display = "none";
     conditionalTextElement.style.display = "block";
-
- 
     AnswerButton.classList.remove("dark");
   } else {
-    // Show the original text and hide the conditional text
     originalTextElement.style.display = "block";
     conditionalTextElement.style.display = "none";
     AnswerButton.classList.add("dark");
-    
   }
+
+  // Clear the previous timeout
+  clearTimeout(updateTimeout);
+
+    // Schedule the quiz update after 1 second
+    updateTimeout = setTimeout(() => {
+      updateQuiz();
+originalTextElement.textContent=  conditionalTextElement.textContent ; // Restore the original text
+      firstChoiceButton.classList.remove("dark"); // Remove the "dark" class
+      AnswerButton.classList.remove("dark"); // Remove the "dark" class
+  
+    }, 1000);
+ 
 });
 
 
 
 
 
-window.addEventListener('load', function() {
-  adjustFooterWidth();
-});
 
-window.addEventListener('resize', function() {
-  adjustFooterWidth();
-});
 
-function adjustFooterWidth() {
-  var containerWidth = document.getElementById('container').offsetWidth;
-  var footer = document.querySelector('.footer');
-  footer.style.width = containerWidth + 'px';
-}
+
+
+
+// const firstChoiceButton = document.querySelector('[data-js="choice-button-1"]');
+// const AnswerButton = document.querySelector('[data-js="active-button"]');
+// const originalTextElement = document.querySelector('.item-text-cbox');
+// const conditionalTextElement = document.querySelector('.text-conditional');
+
+// firstChoiceButton.addEventListener("click", () => {
+//   firstChoiceButton.classList.toggle("dark");
+//   AnswerButton.classList.toggle("dark");
+
+//      // allows text content to be the content I set in the
+//     // html element called data-original-text
+//     // originalTextElement.textContent = originalTextElement.dataset.originalText;
+  
+    
+//   originalTextElement.textContent = originalTextElement.dataset.originalText;
+  
+  
+//   // eventlistener for the color and text toggles
+
+
+//   if (!firstChoiceButton.classList.contains("dark")) {
+//     // Show the conditional text and hide the original text
+//     originalTextElement.style.display = "none";
+//     conditionalTextElement.style.display = "block";
+
+ 
+//     AnswerButton.classList.remove("dark");
+//   } else {
+//     // Show the original text and hide the conditional text
+//     originalTextElement.style.display = "block";
+//     conditionalTextElement.style.display = "none";
+//     AnswerButton.classList.add("dark");
+    
+//   }
+// });
+
+
 
 
 
