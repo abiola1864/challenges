80e638f960e92d286158da479732c752aa80255c
abiola1864 <oyebanjoabiola@gmail.com>
2023-06-07 20:42:12 +0200

    while loop challenge

---
+++ js-loops_while-random-number/index.js
@@ -0,0 +1,13 @@
+console.clear();
+
+// `while` loop
+let number = 0;
+let count = 0;
+
+while (number <= 0.9) {
+  count++;
+  console.log(number);
+  number = Math.random();
+}
+
+console.log(`The number that ended the loop was ${number} and it took ${count} rounds to do this!`);
